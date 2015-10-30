ee59baf0f662179b1045a4b76225f1aa0b09e6fa
Pradyumna Shembekar <pradyumna2905@gmail.com>
2015-10-30 20:39:49 +0000

    Create exercises

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'dashboard/index'
+  
+  resources :users do
+    resources :exercises
+  end
 
-  root 'dashboard#index'
+  root to: 'dashboard#index'
 
 end
