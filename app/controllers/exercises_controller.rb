d36b78043d3c890e4e5797a3a38815e1591e25d2
Pradyumna <pradyumna2905@gmail.com>
2015-11-08 22:56:19 -0800

    Add validation, 1 minor bug

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,36 +1,37 @@
 class ExercisesController < ApplicationController
     before_action :authenticate_user!
     before_action :set_exercise, except: [:index, :new, :create]
-    
-    def index 
-        
+
+    def index
+
     end
-    
+
     def show
-    
+
     end
-    
-    def new 
-       @exercise = current_user.exercises.new 
+
+    def new
+       @exercise = current_user.exercises.new
     end
-    
+
     def create
         @exercise = current_user.exercises.new(exercise_params)
-        
+
         if @exercise.save
             flash[:success] = "Hooray! Your exercise has successfully been added to your lounge"
             redirect_to [current_user, @exercise]
-        else 
+        else
             flash[:danger] = "Oops! Something went wrong"
+            render :new
         end
     end
-    
+
         private
             def exercise_params
                params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
             end
-            
+
             def set_exercise
-                @exercise = current_user.exercises.find(params[:id]) 
+                @exercise = current_user.exercises.find(params[:id])
             end
 end
--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -5,24 +5,46 @@ RSpec.feature 'Creating Exercise' do
        @john = User.create(email: "john@example.com", password: "password")
        login_as(@john)
     end
-    
-    scenario 'with valid inputs' do 
+
+    scenario 'with valid inputs' do
         visit "/"
-        
+
         click_link "My Lounge"
         click_link "New Workout"
-        
+
         expect(page).to have_link("Back")
-        
+
         fill_in "Duration (min)", with: 70
         fill_in "Workout Details", with: "Upper body"
         fill_in "Activity Date", with: "2015-30-10"
-        
+
         click_button "Create Exercise"
-        
+
         expect(page).to have_content("Hooray! Your exercise has successfully been added to your lounge")
-        
+
         exercise = Exercise.last
-        expect(page.current_path).to eq(user_exercise_path(@john, exercise)) 
+        expect(page.current_path).to eq(user_exercise_path(@john, exercise))
+    end
+
+    scenario 'with invalid inputs' do
+        visit "/"
+
+        click_link "My Lounge"
+        click_link "New Workout"
+
+        expect(page).to have_link("Back")
+
+        fill_in "Duration (min)", with: nil
+        fill_in "Workout Details", with: ""
+        fill_in "Activity Date", with: ""
+
+        click_button "Create Exercise"
+
+        expect(page).to have_content("Oops! The exercise cannot be created")
+
+        expect(page).to have_content("Duration cannot be blank")
+        expect(page).to have_content("Duration is not valid")
+        expect(page).to have_content("Workout cannot be blank")
+        expect(page).to have_content("Workout date cannot be blank")
     end
 end
