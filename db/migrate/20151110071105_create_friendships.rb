ad126967c5294429e970ed5ab026c9a53559c1a7
Pradyumna Shembekar <pradyumna2905@gmail.com>
2015-11-10 18:32:27 +0000

    Change migration file to pg  compatible

--- db/migrate/20151110071105_create_friendships.rb
+++ db/migrate/20151110071105_create_friendships.rb
@@ -1,10 +1,19 @@
 class CreateFriendships < ActiveRecord::Migration
-  def change
-    create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+  # def change
+  #   create_table :friendships do |t|
+  #     t.references :user, index: true, foreign_key: true
+  #     t.references :friend, index: true, foreign_key: true
 
+  #     t.timestamps null: false
+  #   end
+  # end
+  
+  def change
+      create_table :friendships do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :friend, index: true
       t.timestamps null: false
     end
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
